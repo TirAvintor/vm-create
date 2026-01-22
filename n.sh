15155431b9f5d69110f1deb97bca71ef3ecaac62
Tir Avintor <TirAvintor@hotmail.com>
2026-01-22 10:42:35 +0200

    Update n.sh

--- n.sh
+++ n.sh
@@ -492,7 +492,7 @@ start_vm() {
         fi
 
         # Add performance enhancements for software emulation
-        screen -S $vm_name qemu_cmd+=(
+        qemu_cmd+=(
             -device virtio-balloon-pci
             -object rng-random,filename=/dev/urandom,id=rng0
             -device virtio-rng-pci,rng=rng0
